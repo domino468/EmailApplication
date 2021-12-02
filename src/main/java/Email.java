f75e91069818a1a314047fe06d31b8c76f4cead0
domino468 <dominik.adam4538@gmail.com>
2021-12-02 21:13:28 +0100

    Finishing Email application app !

--- src/main/java/Email.java
+++ src/main/java/Email.java
@@ -1,3 +1,4 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Email {
@@ -5,24 +6,25 @@ public class Email {
     private String lastName;
     private String password;
     private String department;
-    private int mailboxCapacity;
+    private String email;
+    private int mailboxCapacity = 500;
     private int defaultPasswordLength = 10;
     private String alternateEmail;
+    private String companySuffix = "company.com";
 
     public Email(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
-        System.out.println("EMAIL CREATED :" + this.firstName + " " + this.lastName);
-
         this.department = setDepartment();
-        System.out.println("Department:" + this.department);
-
         this.password = randomPassword(defaultPasswordLength);
-        System.out.println("Your password is: "+ this.password);
+
+        System.out.println("Your password is: " + this.password);
+
+        email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + department + "." + companySuffix;
     }
 
     public String setDepartment() {
-        System.out.print("DEPARTMENT CODES:\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter department code:");
+        System.out.print("New worker: " + firstName + ". DEPARTMENT CODES:\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter department code:");
         Scanner scanner = new Scanner(System.in);
         int depChoice = scanner.nextInt();
         if (depChoice == 1) {
@@ -44,5 +46,34 @@ public class Email {
             password[i] = passwordSet.charAt(rand);
         }
         return new String(password);
+    }
+
+    public void setMailboxCapacity(int capacity) {
+        this.mailboxCapacity = capacity;
+
+    }
+
+    public void setAlternateEmail(String altEmail) {
+        this.alternateEmail = altEmail;
+    }
+
+    public void changePassword(String password) {
+        this.password = password;
+    }
+
+    public int getMailboxCapacity() {
+        return mailboxCapacity;
+    }
+
+    public String getAlternateEmail() {
+        return alternateEmail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String showInfo() {
+        return "\nDISPLAY NAME :" + firstName + " " + lastName + "\nCOMPANY EMAIL: " + email + "\nMAILBOX CAPACITY " + mailboxCapacity + "mb";
     }
 }
--- src/main/java/EmailApplication.java
+++ src/main/java/EmailApplication.java
@@ -1,5 +1,8 @@
 public class EmailApplication {
     public static void main(String[] args) {
         Email email = new Email("Adam","Dominik");
+
+        System.out.println(email.showInfo());
+
     }
 }
