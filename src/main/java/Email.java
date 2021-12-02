deb0adc1cd3ec9c588bbf6fb5ba9347fc3ba9f93
domino468 <dominik.adam4538@gmail.com>
2021-12-02 20:22:18 +0100

    adding and defining emailDepartment

--- src/main/java/Email.java
+++ src/main/java/Email.java
@@ -1,3 +1,33 @@
-public class Email {
+import java.util.Scanner;
 
+public class Email {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String alternateEmail;
+
+    public Email(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("EMAIL CREATED :" + this.firstName + " " + this.lastName);
+
+        this.department = setDepartment();
+    }
+
+    public String setDepartment() {
+        System.out.println("Enter the department\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none");
+        Scanner scanner = new Scanner(System.in);
+        int depChoice = scanner.nextInt();
+        if (depChoice == 1) {
+            return "sales";
+        } else if (depChoice == 2) {
+            return "dev";
+        } else if (depChoice == 3) {
+            return "acct";
+        } else {
+            return "";
+        }
+    }
 }
---
+++ src/main/java/EmailApplication.java
@@ -0,0 +1,5 @@
+public class EmailApplication {
+    public static void main(String[] args) {
+        Email email = new Email("Adam","Dominik");
+    }
+}
